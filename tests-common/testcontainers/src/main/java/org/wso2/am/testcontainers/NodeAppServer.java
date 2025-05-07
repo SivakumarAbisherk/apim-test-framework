c0d917e82df96bfc7b5c66cf1feb69f6e79d6ffb
SivakumarAbisherk <abisherk09@gmail.com>
2025-05-07 08:55:14 +0530

    Implemented Testcontainer components

---
+++ tests-common/testcontainers/src/main/java/org/wso2/am/testcontainers/NodeAppServer.java
@@ -0,0 +1,45 @@
+package org.wso2.am.testcontainers;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.utility.MountableFile;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.time.Duration;
+
+public class NodeAppServer {
+    private static final Logger logger = LoggerFactory.getLogger(NodeAppServer.class);
+    private static final String NODE_SERVER_DIR = "/home/abisherk/Internship/InternProject/apim-test-framework-testcontainers/tests-common/testcontainers/src/main/resources/nodeAppServer";
+
+    private final GenericContainer<?> container;
+
+    public NodeAppServer() {
+        logger.info("Initializing NodeAppServer...");
+
+        // Create a Docker container for the Node.js apps and proxy server
+        container = new GenericContainer<>("node-customer-service")
+                .withExposedPorts(8080)
+                .withNetwork(ContainerNetwork.SHARED_NETWORK)
+                .withNetworkAliases("nodebackend")
+                .waitingFor(Wait.forListeningPort().withStartupTimeout(Duration.ofMinutes(5)));
+
+        // Attach a real-time log consumer
+        Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(logger);
+        container.withLogConsumer(logConsumer);
+        // Start the container
+        container.start();
+        logger.info("NodeAppServer successfully initialized");
+
+    }
+
+    private static class InstanceHolder {
+        private static final NodeAppServer instance = new NodeAppServer();
+    }
+
+    public static NodeAppServer getInstance() {
+        return NodeAppServer.InstanceHolder.instance;
+    }
+}
