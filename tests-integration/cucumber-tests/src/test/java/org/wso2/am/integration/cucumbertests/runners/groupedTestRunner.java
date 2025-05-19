ce2203291dc67b34126a15868a18eabb31d82d2a
SivakumarAbisherk <abisherk09@gmail.com>
2025-05-19 14:54:25 +0530

    Modify the code to run custom test feature dynamically.

--- tests-integration/cucumber-tests/src/test/java/org/wso2/am/integration/cucumbertests/runners/groupedTestRunner.java
+++ tests-integration/cucumber-tests/src/test/java/org/wso2/am/integration/cucumbertests/runners/groupedTestRunner.java
@@ -5,11 +5,14 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.Test;
 
 @CucumberOptions(
-        features = "",
         glue = "org.wso2.am.integration.cucumbertests.stepdefinitions",
         plugin = {"pretty", "html:target/cucumber-report/groupedtestrunner.html"}
 )
 
 @Test(groups = "groupTest")
 public class groupedTestRunner extends AbstractTestNGCucumberTests{
+    static {
+        String featurePath = System.getProperty("feature", "src/test/resources/features/allowedScopes");
+        System.setProperty("cucumber.features", featurePath);
+    }
 }
