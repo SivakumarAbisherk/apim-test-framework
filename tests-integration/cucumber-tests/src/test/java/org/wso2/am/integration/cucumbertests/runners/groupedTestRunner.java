0a3b0752ba46a861df77962cfbeb534e078c484f
SivakumarAbisherk <abisherk09@gmail.com>
2025-05-20 10:45:56 +0530

    Modify code for the feature of last commit

--- tests-integration/cucumber-tests/src/test/java/org/wso2/am/integration/cucumbertests/runners/groupedTestRunner.java
+++ tests-integration/cucumber-tests/src/test/java/org/wso2/am/integration/cucumbertests/runners/groupedTestRunner.java
@@ -5,14 +5,11 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.Test;
 
 @CucumberOptions(
+        features = "src/test/resources/features/api_lifecycle_test.feature",
         glue = "org.wso2.am.integration.cucumbertests.stepdefinitions",
         plugin = {"pretty", "html:target/cucumber-report/groupedtestrunner.html"}
 )
 
 @Test(groups = "groupTest")
 public class groupedTestRunner extends AbstractTestNGCucumberTests{
-    static {
-        String featurePath = System.getProperty("feature", "src/test/resources/features/allowedScopes");
-        System.setProperty("cucumber.features", featurePath);
-    }
 }
